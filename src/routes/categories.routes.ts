2423a8dcbfff78f87bc5b81b52425f9efe9ef4fb
Vinicius Oliveira <viniciusoliveira@Mac-mini-de-Vinicius.local>
2021-08-19 18:28:31 -0300

    Cria rota para listagem de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
@@ -12,6 +11,12 @@ categoriesRoutes.post("/", (request, response) => {
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
